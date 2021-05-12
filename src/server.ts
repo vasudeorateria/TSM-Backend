f9f81942fef2312b96bc02e52097981ee6666c60
Vasudeo Rateria <vasudeorateria98@gmail.com>
2021-05-12 18:25:36 +0530

    final server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ import {paymentRoute} from "./routes/payments";
 const app = express()
 app.use(express.json())
 
-
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
@@ -35,23 +34,3 @@ async function start() {
 
 start()
 
-const {Pool} = require('pg');
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
-
-app.get('/db', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const result = await client.query('SELECT * FROM test_table');
-        const results = {'results': (result) ? result.rows : null};
-        res.render('pages/db', results);
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.send("Error " + err);
-    }
-})
