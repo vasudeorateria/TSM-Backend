33c066c73ba9b46c09ff91bb5b136b2dcfc42887
Vasudeo Rateria <vasudeorateria98@gmail.com>
2021-05-12 19:42:01 +0530

    12/05/21 server

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,14 @@ import {Review} from "./entities/Review";
 import {reviewRoute} from "./routes/reviews";
 import {paymentRoute} from "./routes/payments";
 
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+
 const app = express()
 app.use(express.json())
+
+const http = require('http');
+const { Client } = require('pg');
 
 app.get('/', (req, res) => {
     res.send("Hello World")
@@ -28,6 +34,29 @@ async function start() {
     })
 
     const PORT = process.env.PORT || 3232
+
+    const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+        const client = new Client({
+            connectionString: process.env.DATABASE_URL
+        })
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/plain')
+        client.connect()
+            .then(() => client.query('SELECT * FROM hellotable'))
+            .then((result : any) => {
+                response.end(`${result.rows[0].name}\n`)
+                client.end()
+            })
+            .catch(() => {
+                response.end('ERROR')
+                client.end()
+            })
+    })
+
+    server.listen(PORT, () => {
+        console.log(`Database Server running on ${PORT}/`)
+    })
+
 
     app.listen(PORT, () => {
         console.log("Server started on http://localhost:3232")
