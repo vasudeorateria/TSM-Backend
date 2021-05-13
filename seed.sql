1728e96c92384da84f046307d3c6f78277cc5d1c
Vasudeo Rateria <vasudeorateria98@gmail.com>
2021-05-13 12:04:51 +0530

    basic server

--- seed.sql
+++
@@ -1,2 +0,0 @@
-create table review (id string , image string , review text);
-insert into hellotable values ('Hello World');
--- src/server.ts
+++ src/server.ts
@@ -16,21 +16,21 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
-app.use('/api/reviews', reviewRoute)
-app.use('/api/payments', paymentRoute)
+// app.use('/api/reviews', reviewRoute)
+// app.use('/api/payments', paymentRoute)
 
 async function start() {
 
-    await createConnection({
-        type: 'postgres',
-        username: 'tsm',
-        password: 'tsm',
-        database: 'tsm',
-        entities: [Review],
-        synchronize: true,
-        logging: true,
-        logger: 'advanced-console'
-    })
+    // await createConnection({
+    //     type: 'postgres',
+    //     username: 'tsm',
+    //     password: 'tsm',
+    //     database: 'tsm',
+    //     entities: [Review],
+    //     synchronize: true,
+    //     logging: true,
+    //     logger: 'advanced-console'
+    // })
 
     const PORT = process.env.PORT || 3232
 
